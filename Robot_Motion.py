b9bc1414a5eddb5ec6577a6cceabf61833cc8dd0
leoPuerto01 <104704725+leoPuerto01@users.noreply.github.com>
2022-05-01 13:43:28 -0400

    Update Robot_Motion.py

--- Robot_Motion.py
+++ Robot_Motion.py
@@ -1,7 +1,9 @@
 # Import the Client from ambf_comm package
 # You might have to do: pip install gym
 from ambf_client import Client
+import pandas as pd
 import time
+from 
 
 # Create a instance of the client
 _client = Client()
@@ -14,7 +16,7 @@ _client.connect()
 print(_client.get_obj_names())
 
 # Lets say from the list of printed names, we want to get the
-# handle to an object named "Torus"
+# handle to an object named "Base Link"
 obj = _client.get_obj_handle('baselink')
 time.sleep(3)
 obj.set_pos(0.5,0.5,0.5)
@@ -35,20 +37,25 @@ time.sleep(3)
 # If we have the entire T then 
 # p = [T(1,4), T(2,4), T(3,4)];
 
-# Next we import the data points as a list from the CSV file
+# Next we import the data points as a list from the CSV file using Pandas. 
 
-# pip install pandas
-# import pandas
-# col_list = ["Point1_X","Point1_Y","Point1_Z","Point2_X","Point2_Y","Point2_Z","Point3_X","Point3_Y", "Point3	_Z"]
-# df = pd.read_csv("sample_file.csv", usecols=col_list)
+col_list = ["Point1_X","Point1_Y","Point1_Z","Point2_X","Point2_Y","Point2_Z","Point3_X","Point3_Y", "Point3	_Z"]
+
+# Displaying the entire data frame
+#pd.options.display.max_rows = 9999
+
+#Read the data and organize as a col_list
+df = pd.read_csv("davinci wrist sample.csv", usecols=col_list)
 
 # Store the XYZ values of each point in 1 List , size is nx3 matrix
-#Create 3 lists for 3 points 
+# Create 3 lists for 3 points 
 # I think we will only be using tool tip list as of now
 
-#Multiply the 1,2,3 column with p vector
+# Multiply the 1,2,3 column with p vector
 # Multiply with a scaling factor
 # Hint: Choose the Scaling factor such that the point comes under 2m example 200*x <=  2
 
 # then create a loop that iterates over the list and set pos to (x,y,z)
-#time.sleep (0.2 )
+for i in range(len())
+  # list list and set pos to (x,y,z)
+  time.sleep (0.2)
