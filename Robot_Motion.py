085ec7ebb02ad8dffde7297c28884def9e7484ac
leoPuerto01 <104704725+leoPuerto01@users.noreply.github.com>
2022-05-01 17:36:19 -0400

    Update Robot_Motion.py

--- Robot_Motion.py
+++ Robot_Motion.py
@@ -3,7 +3,6 @@
 from ambf_client import Client
 import pandas as pd
 import time
-from 
 
 # Create a instance of the client
 _client = Client()
@@ -37,24 +36,32 @@ time.sleep(3)
 # If we have the entire T then 
 # p = [T(1,4), T(2,4), T(3,4)];
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Next we import the data points as a list from the CSV file using Pandas. 
 url = "https://raw.githubusercontent.com/leoPuerto01/RBE580_Project/main/matlab/davinci%20wrist%20sample.csv"
 
-col_list = ["Point1_X","Point1_Y","Point1_Z","Point2_X","Point2_Y","Point2_Z","Point3_X","Point3_Y", "Point3	_Z"]
+# Creating a list for coluns headers. This will be important when separating columns
+col_list = ["Point1_X","Point1_Y","Point1_Z","Point2_X","Point2_Y","Point2_Z","Point3_X","Point3_Y", "Point3_Z"]
 
-#Read the data and organize as a col_list
-df = pd.read_csv("davinci wrist sample.csv", sep ='\t', usecols= col_list)
+# Reading the cvs file from github. names=col_list
+df = pd.read_csv(url, engine ='python', sep = ', ', names = col_list)
 
-# The head() method returns a specified number of rows, string from the top.
-# The head() method returns the first 5 rows if a number is not specified.
-# print(df.head())
-# Use to_string() to print the entire DataFrame.
-print(df.to_string())
+# Having issues where all the data is being shown in column 1 only. Therefore, since all this data is in this ne column, I am going to split the column
+column = df.Point1_X
 
-# Store the XYZ values of each point in 1 List , size is nx3 matrix
+# Splitting the only column that contains all the data
+test = column.str.split(',', expand=True)
+
+# Storing the XYZ values of each point in 1 List. Size is n x 3 matrix
+Point1_xyz = np.matrix([test[2], test[3], test[4]])
+Point2_xyz = np.matrix([test[8], test[9], test[10]])
+Point3_xyz = np.matrix([test[14], test[15], test[16]])
+
 # Create 3 lists for 3 points 
 # I think we will only be using tool tip list as of now
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
 # Multiply the 1,2,3 column with p vector
 # Multiply with a scaling factor
 # Hint: Choose the Scaling factor such that the point comes under 2m example 200*x <=  2
